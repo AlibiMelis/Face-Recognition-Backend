81f87cdc4daf97d9df255708f2003f40c60b4f68
Alibi Melis <alibimelis@Alibis-MacBook-Pro.local>
2022-02-03 20:02:33 +0800

    Fixing connection error

--- server.js
+++ server.js
@@ -3,16 +3,18 @@ const cors = require("cors");
 const bcrypt = require("bcrypt");
 const knex = require("knex");
 
-const register = require('./controllers/register');
-const signIn = require('./controllers/signin');
-const profile = require('./controllers/profile');
-const image = require('./controllers/image');
+const register = require("./controllers/register");
+const signIn = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
 
 const db = knex({
   client: "pg",
   connection: {
     connectionString: process.env.DATABASE_URL,
-    ssl: true
+    ssl: {
+      rejectUnauthorized: false,
+    },
   },
 });
 
@@ -21,7 +23,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => res.json('it is working'))
+app.get("/", (req, res) => res.json("it is working"));
 app.post("/signin", signIn.handleSignIn(db, bcrypt));
 app.post("/register", register.handleRegister(db, bcrypt));
 app.get("/profile/:id", profile.handleProfileGet(db));

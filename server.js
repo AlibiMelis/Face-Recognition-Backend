6123abb8f026a423c1069adc31607a5a707729e6
Alibi Melis <alibimelis@Alibis-MacBook-Pro.local>
2022-02-03 14:40:59 +0800

    Connected server to PostgreSQL database

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+const knex = require('knex');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    port : 5432,
+    user : 'Alibi',
+    password : '',
+    database : 'smart-brain'
+  }
+});
+
+console.log(db.select().from('users'));
 
 const app = express();
 

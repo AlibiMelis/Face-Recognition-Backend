af66001c2f0188d43cd69165fd5d062905f0e3bf
Alibi Melis <melis.alibi2000@gmail.com>
2023-02-27 23:13:10 +0800

    Deployment to netlify

---
+++ src/api.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const serverless = require("serverless-http");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+const knex = require("knex");
+
+const register = require("./controllers/register");
+const signIn = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
+
+const db = knex({
+  client: "pg",
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
+});
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+const router = express.Router();
+
+
+router.get("/", (req, res) => res.json("it is working"));
+router.post("/signin", signIn.handleSignIn(db, bcrypt));
+router.post("/register", register.handleRegister(db, bcrypt));
+router.get("/profile/:id", profile.handleProfileGet(db));
+router.put("/image", image.handleImage(db));
+router.post("/imageurl", image.handleApiCall);
+
+app.use(`/.netlify/functions/api`, router);
+
+module.exports = app;
+module.exports.handler = serverless(app);
+
